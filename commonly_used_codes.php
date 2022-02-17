fa089faf62b4aaf68fe064ba5a9af16440e8ae2f
Vishal Vasani <91120316+vishal-gemsdigitalmedia@users.noreply.github.com>
2022-02-17 16:01:29 +0530

    Update commonly_used_codes.php

--- commonly_used_codes.php
+++ commonly_used_codes.php
@@ -27,3 +27,10 @@ $queried_object = get_queried_object();
     );
 
     $tax_terms = get_terms( $args );
+
+@ini_set( 'upload_max_size' , '2048M' );
+@ini_set( 'upload_max_filesize' , '2048M' );
+@ini_set( 'post_max_size', '2048M');
+@ini_set( 'memory_limit', '2048M' );
+@ini_set( 'max_execution_time', '3000' );
+@ini_set( 'max_input_time', '3000' );
