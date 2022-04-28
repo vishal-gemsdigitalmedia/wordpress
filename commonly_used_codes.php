8658655a9538682cf7d0af75593a44074e4db572
Vishal Vasani <91120316+vishal-gemsdigitalmedia@users.noreply.github.com>
2022-04-28 16:02:01 +0530

    Update commonly_used_codes.php

--- commonly_used_codes.php
+++ commonly_used_codes.php
@@ -50,3 +50,31 @@ wc-completed   For Completed
 wc-cancelled   For Cancelled
 wc-refunded   For Refunded
 wc-failed   For Failed
+
+------------------------------------
+
+// Add a new interval of 180 seconds
+// See http://codex.wordpress.org/Plugin_API/Filter_Reference/cron_schedules
+add_filter( 'cron_schedules', 'isa_add_every_three_minutes' );
+function isa_add_every_three_minutes( $schedules ) {
+    $schedules['every_three_minutes'] = array(
+            'interval'  => 180,
+            'display'   => __( 'Every 3 Minutes', 'textdomain' )
+    );
+    return $schedules;
+}
+
+// Schedule an action if it's not already scheduled
+if ( ! wp_next_scheduled( 'isa_add_every_three_minutes' ) ) {
+    wp_schedule_event( time(), 'every_three_minutes', 'isa_add_every_three_minutes' );
+}
+
+// Hook into that action that'll fire every three minutes
+add_action( 'isa_add_every_three_minutes', 'every_three_minutes_event_func' );
+function every_three_minutes_event_func() {
+    // do something
+}
+
+------------------------------------------
+
+
